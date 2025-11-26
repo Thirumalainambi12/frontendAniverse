f5872b53bb0b13da4ae65863b61d146cca351f8d
Thirumalainambi12 <kthirumalainambi39@gmail.com>
2025-11-26 04:14:21 -0800

    Fix path for Render build

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import App from "./animepro/App.jsx";
+import App from "./Animepro/App.jsx";
 import { BrowserRouter} from 'react-router-dom';
 createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>)
